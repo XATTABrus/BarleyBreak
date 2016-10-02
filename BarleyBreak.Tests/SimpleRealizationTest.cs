2d83d64457cea6b9cf429434d11693957f0cb53f
Юрий <xattab.rus@gmail.com>
2016-10-03 02:13:00 +0500

    Добавление проектов в решение.

---
+++ BarleyBreak.Tests/SimpleRealizationTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BarleyBreak.Tests
+{
+    [TestClass]
+    public class SimpleRealizationTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+}
