36ff6fa2540467e5c4eee6fa1d2e053c5aad6f97
Юрий <xattab.rus@gmail.com>
2016-10-04 21:07:45 +0500

    Добавление интерфейса IGame.

---
+++ BarleyBreak.Library/IGame.cs
@@ -0,0 +1,9 @@
+﻿namespace BarleyBreak.Library
+{
+    public interface IGame
+    {
+        int this[int x, int y] { get; }
+        CellPosition GetLocation(int value);
+        IGame Shift(int value);
+    }
+}
