cf68716a081929eeae9b6b3c99f3d5cc5d183dac
Юрий <xattab.rus@gmail.com>
2016-10-06 04:08:22 +0500

    Добавление теста на неизменяемость и комментариев.

--- BarleyBreak.Tests/BaseGameTest.cs
+++ BarleyBreak.Tests/BaseGameTest.cs
@@ -9,6 +9,9 @@ namespace BarleyBreak.Tests
     {
         protected abstract IGame GetGame(params int[] args);
 
+        /// <summary>
+        /// Проверка на корректное создание игрового поля различной площади
+        /// </summary>
         [TestMethod]
         public void CreateGameIsSuccessTest()
         {
@@ -16,13 +19,19 @@ namespace BarleyBreak.Tests
             var game2 = GetGame(1, 2, 3, 4, 5, 6, 7, 8, 0, 9, 10, 11, 12, 13, 14, 15);
         }
 
+        /// <summary>
+        /// Проверка на создание поля с не верными параметрами
+        /// </summary>
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void CreateGameIsFailTest()
         {
             var game = GetGame(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         }
-
+        
+        /// <summary>
+        /// Проверка работы индексатора
+        /// </summary>
         [TestMethod]
         public void IndexaterTest()
         {
@@ -31,12 +40,18 @@ namespace BarleyBreak.Tests
             var result1 = game[1, 1];
             var result2 = game[1, 0];
             var result3 = game[0, 0];
+            var result4 = game.Shift(8)[2, 2];
 
             Assert.AreEqual(6, result1);
             Assert.AreEqual(4, result2);
             Assert.AreEqual(1, result3);
+            Assert.AreEqual(8, result4);
+
         }
 
+        /// <summary>
+        /// Проверка работы метода GetLocation
+        /// </summary>
         [TestMethod]
         public void GetLocationTest()
         {
@@ -44,11 +59,16 @@ namespace BarleyBreak.Tests
 
             var result1 = game.GetLocation(5);
             var result2 = game.GetLocation(0);
+            var result3 = game.Shift(8).GetLocation(0);
 
             Assert.AreEqual(result1, new CellPosition(1, 1));
             Assert.AreEqual(result2, new CellPosition(2, 2));
+            Assert.AreEqual(result3, new CellPosition(2, 1));
         }
 
+        /// <summary>
+        /// Проверка работы метода сдвига с корректными значениями
+        /// </summary>
         [TestMethod]
         public void ShiftIsSuccessTest()
         {
@@ -57,9 +77,13 @@ namespace BarleyBreak.Tests
             var shift1 = game.Shift(8);
             var shift2 = shift1.Shift(5);
             var shift3 = shift2.Shift(4);
-            shift3.Shift(1);
+            var shift4 = shift3.Shift(1);
+            shift4.Shift(1);
         }
 
+        /// <summary>
+        /// Проверка работы метода сдвига с не корректными данными
+        /// </summary>
         [TestMethod]
         [ExpectedException(typeof(InvalidOperationException))]
         public void ShiftIsFailTest()
--- BarleyBreak.Tests/ImmutableGameTest.cs
+++ BarleyBreak.Tests/ImmutableGameTest.cs
@@ -10,5 +10,20 @@ namespace BarleyBreak.Tests
         {
             return new ImmutableGame(args);
         }
+
+        /// <summary>
+        /// Проверка на неизменность объекта
+        /// </summary>
+        [TestMethod]
+        public void ImmutabilityTest()
+        {
+            var game1 = GetGame(1, 2, 3, 4, 5, 6, 7, 8, 0);
+            var game2 = game1.Shift(8);
+            var game3 = game2.Shift(7);
+
+            Assert.AreNotEqual(game1, game2);
+            Assert.AreNotEqual(game1, game3);
+            Assert.AreNotEqual(game2, game3);
+        }
     }
 }
