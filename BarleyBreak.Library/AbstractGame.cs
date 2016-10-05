4d00d5df48dcb99327ff455cc28eabad2e224e92
Юрий <xattab.rus@gmail.com>
2016-10-05 18:38:13 +0500

    Добавление неизменяймой версии игры.

---
+++ BarleyBreak.Library/AbstractGame.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace BarleyBreak.Library
+{
+    public abstract class AbstractGame : IGame
+    {
+        protected Hashtable HtKeyPositonCell; // Таблица, ключ которой позиция ячейки
+        protected Hashtable HtKeyValueCell; // Таблица, ключ которой значение ячейки
+
+        protected AbstractGame() { }
+
+        protected AbstractGame(params int[] args)
+        {
+            CheckArgs(args);
+
+            var sizeSide = (int)Math.Sqrt(args.Length);
+
+            HtKeyPositonCell = new Hashtable();
+            HtKeyValueCell = new Hashtable();
+
+            var counter = 0;
+            // Заполняем хэш-таблицы
+            for (var i = 0; i < sizeSide; i++)
+                for (var j = 0; j < sizeSide; j++)
+                {
+                    HtKeyValueCell.Add(args[counter], new CellPosition(i, j));
+                    HtKeyPositonCell.Add(new CellPosition(i, j).GetHashCode(), args[counter]);
+                    counter++;
+                }
+        }
+
+        public virtual int this[int x, int y] => (int)HtKeyPositonCell[new CellPosition(x, y).GetHashCode()];
+
+        public virtual CellPosition GetLocation(int value)
+        {
+            return (CellPosition)HtKeyValueCell[value];
+        }
+
+        public abstract IGame Shift(int value);
+
+        private void CheckArgs(int[] args)
+        {
+            if (args.Length < 9)
+                throw new ArgumentException("Игра не может состоять меньше чем из 9 элементов!");
+
+            if (!args.ToList().Contains(0))
+                throw new ArgumentException("Игра обязательно должна содержать элемент 0!");
+
+            var sizeSide = Math.Sqrt(args.Length);
+            if (sizeSide - Math.Truncate(sizeSide) != 0)
+                throw new ArgumentException("Не верное коилчество параметров!");
+        }
+
+        protected void SwapCellsWithZero(int value)
+        {
+            var position = (CellPosition)HtKeyValueCell[value];
+            var zeroPosition = (CellPosition)HtKeyValueCell[0];
+
+            // Меняем местами значения ячеек
+            HtKeyValueCell[value] = zeroPosition;
+            HtKeyValueCell[0] = position;
+
+            HtKeyPositonCell[zeroPosition.GetHashCode()] = value;
+            HtKeyPositonCell[position.GetHashCode()] = 0;
+        }
+
+        protected bool NeighboringCellIsZero(int value)
+        {
+            var position = (CellPosition)HtKeyValueCell[value];
+            var zeroPosition = (CellPosition)HtKeyValueCell[0];
+
+            var locationValueSum = position.X + position.Y;
+            var locationZeroSum = zeroPosition.X + zeroPosition.Y;
+            var different = locationValueSum - locationZeroSum;
+
+            return different == -1 || different == 1;
+        }
+    }
+}
--- BarleyBreak.Library/Game.cs
+++ BarleyBreak.Library/Game.cs
@@ -1,93 +1,17 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace BarleyBreak.Library
 {
-    public class Game : IGame
+    public class Game : AbstractGame
     {
-        private readonly Hashtable _htKeyPositonCell; // Таблица, ключ которой позиция ячейки
-        private readonly Hashtable _htKeyValueCell; // Таблица, ключ которой значение ячейки
+        public Game(params int[] args) : base (args) {}
 
-        public Game(params int[] args)
+        public override IGame Shift(int value)
         {
-            if (args.Length < 9)
-                throw new ArgumentException("Игра не может состоять меньше чем из 9 элементов!");
-
-            if(!args.ToList().Contains(0))
-                throw new ArgumentException("Игра обязательно должна содержать элемент 0!");
-
-            var sizeSide = Math.Sqrt(args.Length);   
-            // Проверяем что количество элементов образуют квадрат    
-            if(sizeSide - Math.Truncate(sizeSide) != 0)
-                throw new ArgumentException("Не верное коилчество параметров!");
-
-            _htKeyPositonCell = new Hashtable();
-            _htKeyValueCell = new Hashtable();
-            
-            var counter = 0;
-            // Заполняем хэш-таблицы
-            for(var i = 0; i < sizeSide; i++)
-                for (var j = 0; j < sizeSide; j++)
-                {
-                    _htKeyValueCell.Add(args[counter], new CellPosition(i, j));
-                    _htKeyPositonCell.Add(new CellPosition(i, j).GetHashCode(), args[counter]);
-                    counter++;
-                }           
-        }
-
-        public int this[int x, int y] => (int)_htKeyPositonCell[new CellPosition(x, y).GetHashCode()];
-
-        public CellPosition GetLocation(int value)
-        {
-            return (CellPosition)_htKeyValueCell[value];
-        }
-
-        public IGame Shift(int value)
-        {
-            var position = (CellPosition)_htKeyValueCell[value];
-            
-            var neighboringCells = new List<CellPosition>
-            {
-                // Верхняя соседняя ячейка
-                new CellPosition(position.X - 1, position.Y),
-                // Нижняя соседняя ячейка
-                new CellPosition(position.X + 1, position.Y),
-                // Правая соседняя ячейка
-                new CellPosition(position.X, position.Y + 1),
-                // Левая соседняя ячейка
-                new CellPosition(position.X, position.Y -1)
-            };
-
-            var zeroPosition = new CellPosition(-1, -1);
-
-            foreach (var neighboringCell in neighboringCells)
-            {
-                var valueInCall = -1;
-                try
-                {
-                    valueInCall = (int)_htKeyPositonCell[neighboringCell.GetHashCode()];
-                }
-                catch (Exception)
-                {
-                    continue;
-                }
-
-                if (valueInCall == 0)
-                    zeroPosition = neighboringCell;
-            }
-
-            if(zeroPosition.X == -1 || zeroPosition.Y == -1)
+            if(!NeighboringCellIsZero(value))
                 throw new InvalidOperationException("Все соседние ячейки не нулевые!");
 
-            // Меняем местами значения ячеек
-            _htKeyValueCell[value] = zeroPosition;
-            _htKeyValueCell[0] = position;
-
-            _htKeyPositonCell[zeroPosition.GetHashCode()] = value;
-            _htKeyPositonCell[position.GetHashCode()] = 0;
-
+            SwapCellsWithZero(value);
             return this;
         }
     }
