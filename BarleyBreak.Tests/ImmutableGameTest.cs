76b7f5fbf678f13add5a7d4dc593d857211ce135
Юрий <xattab.rus@gmail.com>
2016-10-07 02:28:55 +0500

    Добавление теста на неизменяемость объекта.

--- BarleyBreak.Tests/ImmutableGameTest.cs
+++ BarleyBreak.Tests/ImmutableGameTest.cs
@@ -12,18 +12,23 @@ namespace BarleyBreak.Tests
         }
 
         /// <summary>
-        /// Проверка на неизменность объекта
+        /// Проверка на неизменность объекта, получаем расположение элемента 8 до сдвига и после сдвига у неизменяемого объекта.
         /// </summary>
         [TestMethod]
         public void ImmutabilityTest()
         {
             var game1 = GetGame(1, 2, 3, 4, 5, 6, 7, 8, 0);
+            var location1 = game1.GetLocation(8);
             var game2 = game1.Shift(8);
+            var location2 = game1.GetLocation(8);
             var game3 = game2.Shift(7);
+            
 
             Assert.AreNotEqual(game1, game2);
             Assert.AreNotEqual(game1, game3);
             Assert.AreNotEqual(game2, game3);
+
+            Assert.AreEqual(location1, location2);
         }
     }
 }
